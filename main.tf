a1ffbf2117f8a676ed4467ca5598ee48779a0fdc
Shayce <shayce50@gmail.com>
2026-02-22 14:21:28 +0000

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform2339" {
+  bucket = "terraform2339"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform2339"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
